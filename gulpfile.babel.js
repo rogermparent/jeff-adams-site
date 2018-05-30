8d8e6100c360d98d8c96e5043ae2a3d2fd6047cf
Roger Parent <rogermparent@gmail.com>
2018-05-29 23:58:14 -0400

    Port site to Victor Hugo

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,6 +6,8 @@ import flatten from "gulp-flatten";
 import postcss from "gulp-postcss";
 import cssImport from "postcss-import";
 import cssnext from "postcss-cssnext";
+import cssExtend from "postcss-extend";
+import cssNested from "postcss-nested";
 import BrowserSync from "browser-sync";
 import webpack from "webpack";
 import webpackConfig from "./webpack.conf";
@@ -30,63 +32,69 @@ gulp.task("build-preview", ["css", "js", "fonts"], (cb) => buildSite(cb, hugoArg
 
 // Compile CSS with PostCSS
 gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
-    .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext()]))
-    .pipe(gulp.dest("./dist/css"))
-    .pipe(browserSync.stream())
+    gulp.src("./src/css/*.css")
+        .pipe(postcss([
+            cssImport({from: "./src/css/main.css"}),
+            cssExtend(),
+            cssNested(),
+            cssnext(),
+            require('postcss-simple-vars'),
+        ]))
+        .pipe(gulp.dest("./dist/css"))
+        .pipe(browserSync.stream())
 ));
 
 // Compile Javascript
 gulp.task("js", (cb) => {
-  const myConfig = Object.assign({}, webpackConfig);
+    const myConfig = Object.assign({}, webpackConfig);
 
-  webpack(myConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
-    browserSync.reload();
-    cb();
-  });
+    webpack(myConfig, (err, stats) => {
+        if (err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString({
+            colors: true,
+            progress: true
+        }));
+        browserSync.reload();
+        cb();
+    });
 });
 
 // Move all fonts in a flattened directory
 gulp.task('fonts', () => (
-  gulp.src("./src/fonts/**/*")
-    .pipe(flatten())
-    .pipe(gulp.dest("./dist/fonts"))
-    .pipe(browserSync.stream())
+    gulp.src("./src/fonts/**/*")
+        .pipe(flatten())
+        .pipe(gulp.dest("./dist/fonts"))
+        .pipe(browserSync.stream())
 ));
 
 // Development server with browsersync
 function runServer() {
-  browserSync.init({
-    server: {
-      baseDir: "./dist"
-    }
-  });
-  gulp.watch("./src/js/**/*.js", ["js"]);
-  gulp.watch("./src/css/**/*.css", ["css"]);
-  gulp.watch("./src/fonts/**/*", ["fonts"]);
-  gulp.watch("./site/**/*", ["hugo"]);
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+    gulp.watch("./src/js/**/*.js", ["js"]);
+    gulp.watch("./src/css/**/*.css", ["css"]);
+    gulp.watch("./src/fonts/**/*", ["fonts"]);
+    gulp.watch("./site/**/*", ["hugo"]);
 };
 
 /**
  * Run hugo and build the site
  */
 function buildSite(cb, options, environment = "development") {
-  const args = options ? hugoArgsDefault.concat(options) : hugoArgsDefault;
+    const args = options ? hugoArgsDefault.concat(options) : hugoArgsDefault;
 
-  process.env.NODE_ENV = environment;
+    process.env.NODE_ENV = environment;
 
-  return spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
-    if (code === 0) {
-      browserSync.reload();
-      cb();
-    } else {
-      browserSync.notify("Hugo build failed :(");
-      cb("Hugo build failed");
-    }
-  });
+    return spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
+        if (code === 0) {
+            browserSync.reload();
+            cb();
+        } else {
+            browserSync.notify("Hugo build failed :(");
+            cb("Hugo build failed");
+        }
+    });
 }
