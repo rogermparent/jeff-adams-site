7b4d3d6e2b5ea60fa5bf37c5406881f4c7b89a4b
Roger Parent <rogermparent@gmail.com>
2018-05-30 19:53:14 -0400

    Remove the photoswipe js import

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,2 @@
 // JS Goes here - ES6 supported
-import photoSwipe from "photoswipe";
-import photoSwipeUI_Default from "photoswipe/dist/photoswipe-ui-default";
 // Say hello
-console.log("🦊 Hello! Edit me in src/js/app.js");
